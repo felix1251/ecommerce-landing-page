1c80deab5e61a22c3819ed3ff2f445f29ddb3b7c
felix1251 <domhack123@gmail.com>
2023-07-15 18:05:07 +0800

    implement location handler

--- router/index.js
+++ router/index.js
@@ -4,4 +4,17 @@ const route = (event) => {
     window.history.pushState({}, "", event.target.href);
 };
 
+const routes = {
+    "/": "/pages/index.html",
+    "/cart": "pages/cart.html",
+    404: "pages/404.html",
+};
+
+const handleLocation = async (event) => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main").innerHTML = html;
+};
+
 window.route = route;
